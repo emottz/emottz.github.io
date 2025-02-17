411e98db87256d04ea67863495e3ea622f5b2161
Emirhan Titiz <emottz199@gmail.com>
2025-02-17 03:06:59 +0300

    copied

---
+++ js/data.js
@@ -0,0 +1,175 @@
+mainData = [
+    { 
+      id: 1,
+      type: "Design",
+      src: 'img/gallery/1.png',
+      tum: 'img/gallery/1.png',
+      cp: ""
+    },
+    { 
+      id: 2,
+      type: "Design",
+      src: 'img/gallery/2.png',
+      tum: 'img/gallery/2.png',
+      cp: ""
+    },
+    { 
+      id: 3,
+      type: "Design",
+      src: 'img/gallery/3.png',
+      tum: 'img/gallery/3.png',
+      cp: ""
+    },
+    { 
+      id: 4,
+      type: "Design",
+      src: 'img/gallery/4.png',
+      tum: 'img/gallery/4.png',
+      cp: ""
+    },
+    { 
+      id: 5,
+      type: "Design",
+      src: 'img/gallery/5.png',
+      tum: 'img/gallery/5.png',
+      cp: ""
+    },
+    { 
+      id: 6,
+      type: "Design",
+      src: 'img/gallery/6.png',
+      tum: 'img/gallery/6.png',
+      cp: ""
+    },
+    { 
+      id: 7,
+      type: "Design",
+      src: 'img/gallery/7.png',
+      tum: 'img/gallery/7.png',
+      cp: ""
+    },
+    { 
+      id: 8,
+      type: "Design",
+      src: 'img/gallery/8.png',
+      tum: 'img/gallery/8.png',
+      cp: ""
+    },
+    { 
+      id: 9,
+      type: "Design",
+      src: 'img/gallery/9.png',
+      tum: 'img/gallery/9.png',
+      cp: ""
+    },{ 
+      id: 10,
+      type: "Design",
+      src: 'img/gallery/10.png',
+      tum: 'img/gallery/10.png',
+      cp: ""
+    },{ 
+      id: 11,
+      type: "Design",
+      src: 'img/gallery/11.png',
+      tum: 'img/gallery/11.png',
+      cp: ""
+    },
+    { 
+      id: 12,
+      type: "Design",
+      src: 'img/gallery/12.png',
+      tum: 'img/gallery/12.png',
+      cp: ""
+    },
+    { 
+      id: 13,
+      type: "Design",
+      src: 'img/gallery/13.png',
+      tum: 'img/gallery/13.png',
+      cp: ""
+    },
+    { 
+      id: 14,
+      type: "Design",
+      src: 'img/gallery/14.png',
+      tum: 'img/gallery/14.png',
+      cp: ""
+    },
+    { 
+      id: 15,
+      type: "Design",
+      src: 'img/gallery/15.png',
+      tum: 'img/gallery/15.png',
+      cp: ""
+    },
+    { 
+      id: 16,
+      type: "Design",
+      src: 'img/gallery/16.jpg',
+      tum: 'img/gallery/16.jpg',
+      cp: ""
+    },
+    { 
+      id: 17,
+      type: "Design",
+      src: 'img/gallery/17.jpg',
+      tum: 'img/gallery/17.jpg',
+      cp: ""
+    },
+    { 
+      id: 18,
+      type: "Game",
+      src: 'img/gallery/18.png',
+      tum: 'img/gallery/18.png',
+      cp: ""
+    },
+    { 
+      id: 19,
+      type: "Game",
+      src: 'img/gallery/19.png',
+      tum: 'img/gallery/19.png',
+      cp: ""
+    },
+    { 
+      id: 20,
+      type: "Game",
+      src: 'img/gallery/20.png',
+      tum: 'img/gallery/20.png',
+      cp: ""
+    },
+    { 
+      id: 21,
+      type: "Game",
+      src: 'img/gallery/21.png',
+      tum: 'img/gallery/21.png',
+      cp: ""
+    },
+    { 
+      id: 22,
+      type: "Game",
+      src: 'img/gallery/22.png',
+      tum: 'img/gallery/22.png',
+      cp: ""
+    },
+    { 
+      id: 23,
+      type: "Game",
+      src: 'img/gallery/23.png',
+      tum: 'img/gallery/23.png',
+      cp: ""
+    },
+    { 
+      id: 24,
+      type: "Game",
+      src: 'img/gallery/24.png',
+      tum: 'img/gallery/24.png',
+      cp: ""
+    },
+    { 
+      id: 25,
+      type: "Game",
+      src: 'img/gallery/25.png',
+      tum: 'img/gallery/25.png',
+      cp: ""
+    },
+]
